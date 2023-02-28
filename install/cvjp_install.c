842f8be0a51ce6b2e65f541e284e71d1992fb77d
Nguyen Minh Nhat Duong <duongcoilc2004@gmail.com>
2023-03-01 03:48:20 +0700

    update file cvjp_install.c

--- install/cvjp_install.c
+++ install/cvjp_install.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+
 #include <string.h>
+
 #include <ctype.h>
+
 #include <stdarg.h>
+
 #include "cvjp.h"
 
 // The function prints to the screen using strings format and supports template literals
